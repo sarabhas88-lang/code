23dd84002a90d879dd576bdf7feeb09191980792
saras <saraswatib609@gmail.com>
2026-04-01 11:33:06 +0530

    these is my first commit

---
+++ 10 Aug/ForEachLoop.java
@@ -0,0 +1,22 @@
+package MapsExample;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int arr[]= new int[3];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+		
+		/*for(int i=0;i<3;i++)
+		{
+			System.out.println(arr[i]);
+		}*/
+
+	}
+
+}
